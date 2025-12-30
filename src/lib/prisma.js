30a6e87fb6c895e2043b8b06e23cd57696c96d94
Eni Fred <enikfred@gmail.com>
2025-12-30 17:13:21 +0100

    fix prisma.js

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,17 +1,38 @@
+// import 'dotenv/config'
+
+//  import { PrismaClient } from '@prisma/client'
+//  import { Pool } from 'pg'
+//  import { PrismaPg } from '@prisma/adapter-pg'
+
+//  const pool = new Pool({
+//    connectionString: process.env.DATABASE_URL,
+//  })
+
+//  const adapter = new PrismaPg(pool)
+
+//  const prisma = new PrismaClient({
+//    adapter,
+//  })
+
+//  export default prisma
+
 import 'dotenv/config'
 
- import { PrismaClient } from '@prisma/client'
- import { Pool } from 'pg'
- import { PrismaPg } from '@prisma/adapter-pg'
+import prismaPkg from '@prisma/client'
+import pgPkg from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
- const pool = new Pool({
-   connectionString: process.env.DATABASE_URL,
- })
+const { PrismaClient } = prismaPkg
+const { Pool } = pgPkg
 
- const adapter = new PrismaPg(pool)
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
 
- const prisma = new PrismaClient({
-   adapter,
- })
+const adapter = new PrismaPg(pool)
 
- export default prisma
+const prisma = new PrismaClient({
+  adapter,
+})
+
+export default prisma
